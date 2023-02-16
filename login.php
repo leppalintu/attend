80a299914cdc0742c36b72d43ae6b3cc074c183c
leppalintu <63555681+leppalintu@users.noreply.github.com>
2023-02-16 15:03:29 +0200

    login

---
+++ login.php
@@ -0,0 +1,38 @@
+<?php
+	require_once 'includes/header.php';	
+	require_once 'db/conn.php';
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		$username = strtolower(trim($_POST['username']));
+		$password = $_POST['password'];
+		$new_password = md5($password.$username);
+		$result = $user->getUser($username,$new_password);
+		if (!$result) {
+			echo '<div class="alert">Kasutaja nimi või parool on vale. </div>';	
+		}else {
+			$_SESSION['username'] = $username;
+			$_SESSION['userid'] = $result['id'];
+#	header("Location:viewrecords.php");
+		}
+	}
+	$title= 'User login';
+?>
+
+<h1 class="text-center"><?php echo $title ?> </h1>
+	<form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
+		<table class="table table-sm">
+			<tr>
+				<td><label for="username">Username: * </label></td>
+				<td><input type="text" name="username" class="form-control" id="username" value="<?php if($_SERVER['REQUEST_METHOD'] == 'POST') echo $_POST['username']; ?>">
+				</td>
+			</tr>
+			<tr>
+				<td><label for="password">Password:*</label></td>
+				<td><input type="password" name="password" class="form-control" id="password">
+				</td>
+			</tr>
+		</table>
+		<input type="submit" value="login" class="btn btn-primary btn-block">
+	</form>
+
+
+<?php include_once 'includes/footer.php';
