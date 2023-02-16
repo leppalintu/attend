04d6ce97978cc97200add814939688e2a4d68b64
leppalintu <63555681+leppalintu@users.noreply.github.com>
2023-02-16 16:25:56 +0200

    login.php testing

--- login.php
+++ login.php
@@ -1,20 +1,41 @@
 <?php
-	require_once 'includes/header.php';	
-	require_once 'db/conn.php';
-	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-		$username = strtolower(trim($_POST['username']));
-		$password = $_POST['password'];
-		$new_password = md5($password.$username);
-		$result = $user->getUser($username,$new_password);
-		if (!$result) {
-			echo '<div class="alert">Kasutaja nimi või parool on vale. </div>';	
-		}else {
-			$_SESSION['username'] = $username;
-			$_SESSION['userid'] = $result['id'];
+#	require_once 'includes/header.php';	
+#	require_once 'db/conn.php';
+#	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+#		$username = strtolower(trim($_POST['username']));
+#		$password = $_POST['password'];
+#		$new_password = md5($password.$username);
+#		$result = $user->getUser($username,$new_password);
+#		if (!$result) {
+#			echo '<div class="alert">Kasutaja nimi või parool on vale. </div>';	
+#		}else {
+#			$_SESSION['username'] = $username;
+#			$_SESSION['userid'] = $result['id'];
 #	header("Location:viewrecords.php");
-		}
-	}
-	$title= 'User login';
+#		}
+#	}
+#	$title= 'User login';
+    $title = 'User Login'; 
+
+    require_once 'includes/header.php'; 
+    require_once 'db/conn.php'; 
+    
+    //If data was submitted via a form POST request, then...
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $username = strtolower(trim($_POST['username']));
+        $password = $_POST['password'];
+        $new_password = md5($password.$username);
+
+        $result = $user->getUser($username,$new_password);
+        if(!$result){
+            echo '<div class="alert alert-danger">Username or Password is incorrect! Please try again. </div>';
+        }else{
+            $_SESSION['username'] = $username;
+            $_SESSION['userid'] = $result['id'];
+            header("Location: viewrecords.php");
+        }
+
+    }
 ?>
 
 <h1 class="text-center"><?php echo $title ?> </h1>
