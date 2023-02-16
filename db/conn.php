9ab2980ba726a45dc18a83c59463d6d7edb500db
leppalintu <63555681+leppalintu@users.noreply.github.com>
2023-02-16 13:04:59 +0200

    andmebaas

---
+++ db/conn.php
@@ -0,0 +1,14 @@
+<?php
+	$host = 'localhost';
+	$db = 'kiisu';
+	$user = 'kutsu';
+	$pass = 'koerasaba';
+
+	$dsn = "mysql:host=$host;dbname=$db";
+	try{
+		$pdo = new PDO($dsn, $user, $pass);
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch(PDOException $e){
+		throw new PDOException($e->getMessage());
+	}
+?>
